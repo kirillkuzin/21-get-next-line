b662e3dc42bf4958592a41cc12103296bd6be48a
Greenbeard Geordi <ggeordi@ox-r1.21-school.ru>
2019-10-10 21:16:06 +0300

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -16,65 +16,55 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFF_SIZE 4096
+#define BUFF_SIZE 6
+
+size_t		scan_buff(char *buffer)
+{
+    size_t		i;
+
+    i = 0;
+    while (i < BUFF_SIZE && buffer[i])
+    {
+        if (buffer[i] == '\n')
+            return (i);
+        i++;
+    }
+    return (0);
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	char    sym;
-	size_t	line_len;
-	size_t	buff_count;
-	size_t  i;
-	int     eof;
+    char	buffer[BUFF_SIZE];
+    size_t	line_len;
+    size_t	buff_count;
 
-	line_len = 0;
-	i = 0;
-    sym = 1;
-	while (sym != '\n' && read(fd, &sym, 1))
-		line_len++;x
-	if (sym != '\n')
-	    line_len++;
-	lseek(fd, -line_len, SEEK_CUR);
-	*line = (char*)malloc(sizeof(char) * line_len);
-	buff_count = (line_len - 1) / BUFF_SIZE;
-	if ((line_len - 1) % BUFF_SIZE > 0) buff_count++;
-	printf("%zu\n", line_len);
-    while (buff_count > 0)
+    buff_count = 0;
+    read(fd, &buffer, BUFF_SIZE);
+    line_len = scan_buff(&buffer[0]);
+    while (line_len == 0)
     {
-	    if (buff_count == 1 && (line_len - 1) % BUFF_SIZE > 0)
-        {
-            read(fd, &line[0][i], (line_len - 1) % BUFF_SIZE);
-            i += (line_len - 1) % BUFF_SIZE;
-        }
-	    else
-        {
-            eof = read(fd, &line[0][i], BUFF_SIZE);
-            i += BUFF_SIZE;
-	    }
-	    buff_count--;
+        buff_count++;
+        read(fd, &buffer, BUFF_SIZE);
+        line_len = scan_buff(&buffer[0]);
     }
-//    line[0][line_len - 1] = '\0';
-    printf("%c", sym);
-    if (sym != '\0')
-        lseek(fd, 1, SEEK_CUR);
+    *line = (char*)malloc(sizeof(char) * (BUFF_SIZE * buff_count + line_len + 1));
+    line[0][BUFF_SIZE * buff_count + line_len] = '\0';
+    strncpy(line[0], &buffer[0], (size_t)(BUFF_SIZE * buff_count + line_len));
     printf("%s", line[0]);
-    return (1);
+    return (0);
 }
 
 int		main(int argc, char **argv)
 {
-	int		state;
-	int		fd;
-	char	**line;
+    int		state;
+    int		fd;
+    char	**line;
 
-	state = 0;
-	fd = open(argv[1], O_RDONLY);
-	line = (char**)malloc(sizeof(char*) * 100);
-	while (state < 2)
-	{
-	    get_next_line(fd, line);
-        line++;
-        state++;
-    }
-	close(fd);
-	return (1);
+    state = 1;
+    fd = open(argv[1], O_RDONLY);
+    line = (char**)malloc(sizeof(char*) * 100);
+    while (state == 1)
+        state = get_next_line(fd, line);
+    close(fd);
+    return (1);
 }
